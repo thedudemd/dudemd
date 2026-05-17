fb1944dff4dc14121b4c59976306f6c1532bca44
thedudemd <thedudemd@gmail.com>
2026-05-17 14:59:51 -0700

    fix admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,7 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = { title: "DudeMD Admin" }
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>
 }
