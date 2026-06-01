f95909bc549960d6d7bfbf5615027ba30841042b
thedudemd <thedudemd@gmail.com>
2026-06-01 03:14:16 -0700

    update favicon link to png

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
       <head>
-          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="icon" href="/favicon.png" type="image/png" />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9224150605844856" crossOrigin="anonymous"></script>
           <meta name='facebook-domain-verification' content='at3t423mnp96egxz0gammrzkx46nie' />
         </head>
