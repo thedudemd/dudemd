5b67aa094bd40a38f5fa1499d3c868781ab505a6
thedudemd <thedudemd@gmail.com>
2026-06-01 01:40:08 -0700

    fix Meta Pixel script loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,6 +87,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </head>
       <body style={{ backgroundColor: '#f7f4ee', color: '#0e1a2b' }}>
         <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
           fbq('init', '2107832130079548');
           fbq('track', 'PageView');
         ` }} />
