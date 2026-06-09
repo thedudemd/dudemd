974f1b479f44ee7155f2596614d30e54595ece14
thedudemd <thedudemd@gmail.com>
2026-06-09 00:43:09 -0700

    Revert "fix: restore navy band on homepage"
    This reverts commit 7042cc49bf382f9f4236051c8d57111899a9a7d2.

--- app/page.tsx
+++ app/page.tsx
@@ -163,9 +163,6 @@ export default async function HomePage() {
         </div>
       
 
-      {/* WELLNESS BAND */}
-      <section style={{ backgroundColor: "var(--color-navy)", padding: "3rem 0" }} />
-
       {/* LATEST GRID */}
       <section style={{ padding: '3rem 0', borderBottom: '1px solid var(--color-border)' }}>
         <div className="container-content">
