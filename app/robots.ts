18ab518078a83c015accf6b1ccfe7317ab48ab24
thedudemd <thedudemd@gmail.com>
2026-06-15 01:01:51 -0700

    Fix robots.txt: allow Googlebot to crawl AdSense ad scripts

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,18 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin/', '/api/', '/account/'],
-    },
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/admin/', '/account/'],
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/account/'],
+      },
+    ],
     sitemap: 'https://www.dudemd.com/sitemap.xml',
   }
 }
